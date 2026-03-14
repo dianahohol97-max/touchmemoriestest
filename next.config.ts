8364a76a4862d6ec02938b3888ba186b256f57a4
dianahohol97-max <dianahohol97@gmail.com>
2026-03-14 06:26:58 +0200

    fix(constructor): strictly follow user instructions for output and constructor standardization

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -21,6 +19,6 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
